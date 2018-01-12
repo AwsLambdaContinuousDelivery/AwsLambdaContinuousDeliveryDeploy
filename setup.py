cda12a23063f8e98dc20f986fe7fc194734dfeb4
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2018-01-12 22:41:27 +0100

    Fix Merge Conflicts

--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@
 
 from distutils.core import setup
 
-<<<<<<< HEAD
 setup( name='AwsLambdaContinuousDeliveryDeploy'
      , version = '0.0.1'
      , description = 'AwsLambdaContinuousDeliveryDeploy'
-=======
-setup( name='pyAwsLambdaContinuousDeliveryDeploy'
-     , version = '0.0.1'
-     , description = 'pyAwsLambdaContinuousDeliveryDeploy'
->>>>>>> 87acc6068dbe565a352525bb476bae6a84adb6a1
      , author = 'Janos Potecki'
      , url = 'https://github.com/AwsLambdaContinuousDelivery/AwsLambdaContinuousDeliveryDeploy'
      , packages = ['awslambdacontinuousdelivery.deploy']
@@ -19,8 +13,5 @@ setup( name='pyAwsLambdaContinuousDeliveryDeploy'
           'troposphere'
         , 'awacs'
         ]
-<<<<<<< HEAD
      )
-=======
-     )
->>>>>>> 87acc6068dbe565a352525bb476bae6a84adb6a1
+
